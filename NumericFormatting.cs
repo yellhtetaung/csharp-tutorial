b77186ee76a0d56b8b1b38ef7ee8187513445caf
yellhtetaung <yehtetaung.dev@gmail.com>
2024-12-25 16:02:24 +0630

    Add NumericFormatting

--- NumericFormatting.cs
+++ NumericFormatting.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace MyFirstProject
 {
@@ -10,6 +11,26 @@ namespace MyFirstProject
     {
         static void Main(string[] args)
         {
+            double value = 1000D / 12.34;
+
+            Console.WriteLine(value);
+            Console.WriteLine(string.Format("{0:0}", value));
+            Console.WriteLine(string.Format("{0:0.0}", value));
+            Console.WriteLine(string.Format("{0:0.00}", value));
+
+            double money = -10D / 3D;
+            Console.WriteLine(money);
+            Console.WriteLine(string.Format("-£10 / £3 = £{0:0.00}", money));
+            Console.WriteLine(money.ToString("C"));
+            Console.WriteLine(money.ToString("C0"));
+            Console.WriteLine(money.ToString("C1"));
+            Console.WriteLine(money.ToString("C2"));
+
+            Console.WriteLine(money.ToString("C", CultureInfo.CurrentCulture));
+            Console.WriteLine(money.ToString("C", CultureInfo.CreateSpecificCulture("en-GB")));
+            Console.WriteLine(money.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
+            Console.WriteLine(money.ToString("C", CultureInfo.CreateSpecificCulture("en-AU")));
+
             Console.ReadLine();
         }
     }
