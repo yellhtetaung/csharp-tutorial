efbe21593a380fb12ea7c09606277dd7c5bb1f9a
yellhtetaung <yehtetaung.dev@gmail.com>
2024-12-25 14:43:05 +0630

    Add NumericFormatting

---
+++ NumericFormatting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject
+{
+    internal class NumericFormatting
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
